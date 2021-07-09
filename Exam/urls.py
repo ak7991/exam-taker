671a4558b0c51818c88e4d9ac7f113043cddd09b
Utkarsh-TG <utkarshtyagi1312@gmail.com>
2021-07-09 20:03:20 +0530

    Second Commit

--- Exam/urls.py
+++ Exam/urls.py
@@ -5,6 +5,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
     path('', include('main.urls')),
 ]
